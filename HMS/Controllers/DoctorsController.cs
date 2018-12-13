e9f36077bbb5cc4ac2afd236e3d44e6ceaf67f48
aliarsalan-se <aliarsalan.se@gmail.com>
2018-12-13 09:42:13 +0500

    Front Templates Completed for Forms

---
+++ HMS/Controllers/DoctorsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HMS.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        // GET: Doctors
+        [HttpGet]
+       public JsonResult GetDoctors()
+        {
+            var doctors = new List<string>();
+            doctors.Add("Peter");
+            doctors.Add("John Snow");
+            doctors.Add("Oliver");
+            return Json(doctors, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ HMS/Controllers/PatientController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HMS.Controllers
+{
+    public class PatientController : Controller
+    {
+        // GET: Patient
+      public ActionResult AddPatient()
+        {
+            return View();
+        }
+     [HttpPost]
+        public JsonResult Test(string Emp)
+        {
+
+            User user = new User();
+            user.FirstName = "10/02/1995";
+            user.LastName = "Male";
+           
+
+            return Json(user, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    internal class User
+    {
+        public String FirstName { get; set; }
+        public String LastName{ get; set; }
+    }
+}
